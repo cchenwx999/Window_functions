9ed8536b3c20d834a53c5706f55b8f2071bb46a3
cchenwx999 <49959759+cchenwx999@users.noreply.github.com>
2019-04-26 14:40:17 -0700

    Update Over()_Partition_by.sql

--- Over()_Partition_by.sql
+++ Over()_Partition_by.sql
@@ -1,6 +1,11 @@
 ### Window Functions Introduction
 -- from [Drill.Apache](https://drill.apache.org/docs/sql-window-functions-introduction/)
 
+The OVER() clause has the following capabilities:
+
+  Defines window partitions to form groups of rows. (PARTITION BY clause)
+  Orders rows within a partition. (ORDER BY clause)
+  
 -- compare the Avg() Over() with Avg()
 
 select avg(sales) as avgsales from q1_sales;
@@ -11,7 +16,7 @@ select avg(sales) as avgsales from q1_sales;
    +-----------+
 1 row selected (0.131 seconds)
 
-   select emp_name, dealer_id, sales, avg(sales) over() as avgsales from q1_sales;
+select emp_name, dealer_id, sales, avg(sales) over() as avgsales from q1_sales;
    +-----------------+------------+--------+-----------+
    |    emp_name     | dealer_id  | sales  | avgsales  |
    +-----------------+------------+--------+-----------+
@@ -27,3 +32,39 @@ select avg(sales) as avgsales from q1_sales;
    | Haviva Montoya  | 2          | 9308   | 13631     |
    +-----------------+------------+--------+-----------+
    10 rows selected (0.213 seconds)
+
+
+ select emp_name, dealer_id, sales, avg(sales) over (partition by dealer_id) as avgsales from q1_sales;
+    +-----------------+------------+--------+-----------+
+   |    emp_name     | dealer_id  | sales  | avgsales  |
+   +-----------------+------------+--------+-----------+
+   | Ferris Brown    | 1          | 19745  | 14357     |
+   | Noel Meyer      | 1          | 19745  | 14357     |
+   | Raphael Hull    | 1          | 8227   | 14357     |
+   | Jack Salazar    | 1          | 9710   | 14357     |
+   | Beverly Lang    | 2          | 16233  | 13925     |
+   | Kameko French   | 2          | 16233  | 13925     |
+   | Haviva Montoya  | 2          | 9308   | 13925     |
+   | Ursa George     | 3          | 15427  | 12368     |
+   | Abel Kim        | 3          | 12369  | 12368     |
+   | May Stout       | 3          | 9308   | 12368     |
+   +-----------------+------------+--------+-----------+
+   10 rows selected (0.215 seconds)  
+   
+ select dealer_id, sales, emp_name,row_number() over (partition by dealer_id order by sales) as `row`,
+    avg(sales) over (partition by dealer_id) as avgsales from q1_sales;
+    +------------+--------+-----------------+------+---------------+
+   | dealer_id  | sales  |    emp_name     | row  |      avgsales |
+   +------------+--------+-----------------+------+---------------+
+   | 1          | 8227   | Raphael Hull    | 1    | 14356         |
+   | 1          | 9710   | Jack Salazar    | 2    | 14356         |
+   | 1          | 19745  | Ferris Brown    | 3    | 14356         |
+   | 1          | 19745  | Noel Meyer      | 4    | 14356         |
+   | 2          | 9308   | Haviva Montoya  | 1    | 13924         |
+   | 2          | 16233  | Beverly Lang    | 2    | 13924         |
+   | 2          | 16233  | Kameko French   | 3    | 13924         |
+   | 3          | 9308   | May Stout       | 1    | 12368         |
+   | 3          | 12369  | Abel Kim        | 2    | 12368         |
+   | 3          | 15427  | Ursa George     | 3    | 12368         |
+   +------------+--------+-----------------+------+---------------+
+   10 rows selected (0.37 seconds)
